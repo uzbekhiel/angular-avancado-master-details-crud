2264d01367ada3b37a88091ed35931c7c4be246b
Saulo dos Santos Firmino <uzbekhiel@gmail.com>
2020-04-14 15:33:33 -0300

    project restructuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDatabase } from './in-memory-databae';
-import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
