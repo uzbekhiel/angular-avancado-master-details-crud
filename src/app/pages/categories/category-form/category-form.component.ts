93371aa2388d35fe8da3ae610bf1c7b3dcb78508
Saulo dos Santos Firmino <uzbekhiel@gmail.com>
2020-04-14 15:42:49 -0300

    adding base resource model

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -107,5 +107,4 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     );
   }
 
-
 }
--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -29,7 +29,4 @@ export class CategoryListComponent implements OnInit {
       );
     }
   }
-
-
-
 }
