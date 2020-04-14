46d42d4780fd1b5cec30ff3bb9f6eaae0c623374
Saulo dos Santos Firmino <uzbekhiel@gmail.com>
2020-04-13 23:31:51 -0300

    setting up entries module

---
+++ src/app/models/entry.model.ts
@@ -0,0 +1,24 @@
+import { Category } from './category.model';
+
+export class Entry {
+    constructor(
+        public id?: number,
+        public name?: string,
+        public description?: string,
+        public type?: string,
+        public amount?: string,
+        public date?: string,
+        public paid?: boolean,
+        public categoryId?: boolean,
+        public category?: Category
+    ) { }
+
+    static types = {
+        expense: 'Despesa',
+        reneveu: 'Receita'
+    }
+
+    get paidText(): string {
+        return this.paid ? 'Pago' : 'Pendente';
+    }
+}
---
+++ src/app/services/entry.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, flatMap } from 'rxjs/operators';
+import { Entry } from '../models/entry.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class EntryService {
+
+    private apiPath = 'api/entries';
+
+    constructor(private http: HttpClient) { }
+
+    getAll(): Observable<Entry[]> {
+        return this.http.get(this.apiPath).pipe(
+            catchError(this.handleError),
+            map(this.jsonDataToEntries)
+        );
+    }
+
+    getById(id: number): Observable<Entry> {
+        const url = `${this.apiPath}/${id}`;
+        return this.http.get(url).pipe(
+            catchError(this.handleError),
+            map(this.jsonDataToEntry)
+        );
+    }
+
+    create(entry: Entry): Observable<Entry> {
+        return this.http.post(this.apiPath, entry).pipe(
+            catchError(this.handleError),
+            map(this.jsonDataToEntry)
+        );
+    }
+
+    update(entry: Entry): Observable<Entry> {
+        const url = `${this.apiPath}/${entry.id}`;
+        return this.http.put(url, entry).pipe(
+            catchError(this.handleError),
+            map(() => entry)
+        );
+    }
+
+    delete(id: number): Observable<any> {
+        const url = `${this.apiPath}/${id}`;
+        return this.http.delete(url).pipe(
+            catchError(this.handleError),
+            map(() => null)
+        );
+    }
+
+    private jsonDataToEntry(jsonObject: any): Entry {
+        return jsonObject as Entry;
+    }
+
+    private jsonDataToEntries(jsonObject: any[]): Entry[] {
+        const entries: Entry[] = [];
+        jsonObject.forEach(e => { entries.push(e as Entry); });
+        return entries;
+    }
+
+    private handleError(error: any): Observable<any> {
+        console.log('Erro na requisição => ', error);
+        return throwError(error);
+    }
+
+
+}
